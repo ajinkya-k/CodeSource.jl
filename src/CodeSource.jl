c87e3dfa1d458055b99d76c1c9a0ce4605d698ba
Ajinkya Kokandakar <ajinkyakokandakar@gmail.com>
2025-06-12 15:55:12 -0500

    zeroth working version with tests

--- src/CodeSource.jl
+++ src/CodeSource.jl
@@ -1,5 +1,20 @@
 module CodeSource
 
-# Write your package code here.
+using InteractiveUtils
+using Crayons
+using JuliaSyntax
+using CodeTracking
+using OhMyREPL
 
+export @code_src
+
+macro code_src(expr...)
+    codestr = InteractiveUtils.gen_call_with_extracted_types_and_kwargs(CodeTracking, :code_string, expr)
+    quote
+        tokens = collect(JuliaSyntax.tokenize($codestr))
+        crayons = fill(Crayon(), length(tokens))
+        OhMyREPL.Passes.SyntaxHighlighter.SYNTAX_HIGHLIGHTER_SETTINGS(crayons, tokens, 0, $codestr)
+        OhMyREPL.untokenize_with_ANSI(crayons, tokens, $codestr)
+    end
+end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
-using CodeSource
-using Test
+using Test, OhMyREPL, CodeTracking, CodeSource
 
-@testset "CodeSource.jl" begin
-    # Write your tests here.
+@testset "code_src macro" begin
+    original_stdout = stdout
+
+    (rd, wr) = redirect_stdout()
+
+    @code_src sum(1:3)
+    redirect_stdout(original_stdout)
+    close(wr)
+
+    s = read(rd, String)
+    redirect_stdout(original_stdout)
+    @test occursin("\e[", s)
+    @test occursin("AbstractRange", s)
 end
