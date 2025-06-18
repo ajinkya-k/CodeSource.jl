328ba96a1b4dcc701211d244baca435d9e110eab
Ajinkya Kokandakar <ajinkyakokandakar@gmail.com>
2025-06-18 16:05:16 -0500

    add modules to makedocs

--- docs/make.jl
+++ docs/make.jl
@@ -3,10 +3,10 @@ using DocumenterVitepress
 using CodeSource
 
 makedocs(;
-    # modules = [CodeSource],
-    # repo = Remotes.GitHub("ajinkya-k", "CodeSource.jl"),
-    # authors = "Jerry Ling <proton@jling.dev>, Ajinkya <ajinkyakokandakar@gmail.com> and contributors",
-    # sitename = "CodeSource.jl",
+    modules = [CodeSource],
+    repo = Remotes.GitHub("ajinkya-k", "CodeSource.jl"),
+    authors = "Jerry Ling <proton@jling.dev>, Ajinkya <ajinkyakokandakar@gmail.com> and contributors",
+    sitename = "CodeSource.jl",
     format = DocumenterVitepress.MarkdownVitepress(
         repo = "https://github.com/ajinka-k/CodeSource.jl",
     ),
