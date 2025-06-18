7827da3e3a2c67a33d45c4122a655970223139ff
Ajinkya Kokandakar <ajinkya-k@users.noreply.github.com>
2025-06-18 16:23:58 -0500

    add documentation (#3)
    Add documentation for the CodeSource package v0.1.0

---
+++ docs/make.jl
@@ -0,0 +1,23 @@
+using Documenter
+using DocumenterVitepress
+using CodeSource
+
+makedocs(;
+    modules = [CodeSource],
+    repo = Remotes.GitHub("ajinkya-k", "CodeSource.jl"),
+    authors = "Jerry Ling <proton@jling.dev>, Ajinkya <ajinkyakokandakar@gmail.com> and contributors",
+    sitename = "CodeSource.jl",
+    format = DocumenterVitepress.MarkdownVitepress(
+        repo = "https://github.com/ajinka-k/CodeSource.jl",
+    ),
+    pages = [
+        "index.md"
+    ],
+    clean = true
+)
+
+DocumenterVitepress.deploydocs(;
+    repo = "github.com/ajinkya-k/CodeSource.jl", # this must be the full URL!
+    devbranch = "main",
+    push_preview = true,
+)
