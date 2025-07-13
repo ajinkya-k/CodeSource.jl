46cd15a1b2f41f79aeabf453f1a25795a47e2c6a
Ajinkya Kokandakar <ajinkyakokandakar@gmail.com>
2025-07-12 20:37:39 -0500

    fixed author names

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,7 @@ DocMeta.setdocmeta!(CodeSource, :DocTestSetup, :(using CodeSource); recursive=tr
 makedocs(;
     modules = [CodeSource],
     repo = Remotes.GitHub("ajinkya-k", "CodeSource.jl"),
-    authors = "Jerry Ling <proton@jling.dev>, Ajinkya <ajinkyakokandakar@gmail.com> and contributors",
+    authors = "Jerry Ling <proton@jling.dev>, Ajinkya Kokandakar<ajinkyakokandakar@gmail.com> and contributors",
     sitename = "CodeSource.jl",
     format = DocumenterVitepress.MarkdownVitepress(;
         repo = "https://github.com/ajinkya-k/CodeSource.jl",
@@ -19,7 +19,7 @@ makedocs(;
     clean = true,
 )
 
-deploydocs(;
+DocumenterVitepress.deploydocs(;
     repo = "github.com/ajinkya-k/CodeSource.jl",
     target = "build", # this is where Vitepress stores its output
     devbranch = "main",
